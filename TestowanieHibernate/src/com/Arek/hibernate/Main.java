24bf316a583dd45d847387b63afe948c106e881e
Arek <ccaliencc@gmail.com>
2018-07-24 12:21:15 +0200

    1

--- TestowanieHibernate/src/com/Arek/hibernate/Main.java
+++ TestowanieHibernate/src/com/Arek/hibernate/Main.java
@@ -1,37 +1,91 @@
 package com.Arek.hibernate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
 
 public class Main
 {
 
 	public static void main(String[] args)
 	{
-	
-		Student student = new Student();
-		
-		student.setStudent("Arek1");
-		
-		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
 
-		session.save(student);
-		student.setStudent("Arek updated2");
-//		student = (Student) session.get(Student.class, 1); // 1 oznacza ID numer który chcemy zmienić
-//		System.out.println("Student Object having student name as: " + student.getStudent());
-//		
-		
-//		
-		//Przekazuje wybrany rekord z tabeli zap omocą get(Stduent.class, 1) do dalszej obróbki, czy to update czy delete
-//		session.update(student);
-		//session.delete(student);
-		session.getTransaction().commit();
-		session.close();
-		sessionFactory.close();
+		//		Student student = new Student();
+		//		
+		//		student.setStudent("Arek1");
+		//		
+		//		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		//		Session session = sessionFactory.openSession();
+		//		session.beginTransaction();
+		//
+		//		session.save(student);
+		//		student.setStudent("Arek updated2");
+		////		student = (Student) session.get(Student.class, 1); // 1 oznacza ID numer który chcemy zmienić
+		////		System.out.println("Student Object having student name as: " + student.getStudent());
+		////		
+		//		
+		////		
+		//		//Przekazuje wybrany rekord z tabeli zap omocą get(Stduent.class, 1) do dalszej obróbki, czy to update czy delete
+		////		session.update(student);
+		//		//session.delete(student);
+		//		session.getTransaction().commit();
+		//		session.close();
+		//		sessionFactory.close();
 
+		List<Student> messages = new ArrayList<Student>();
+
+		try
+		{
+			SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+			Session session = sessionFactory.openSession();
+			session.beginTransaction();
+			System.out.println("po konfiguracji");
+			//session.beginTransaction();
+			Query<Student> query = session.createQuery("From Message");
+
+			messages = query.getResultList();
+			session.getTransaction().commit();
+			session.close();
+		}
+		catch (Throwable th)
+		{
+			System.err.println("Enitial SessionFactory creation failed" + th);
+			throw new ExceptionInInitializerError(th);
+
+		}
+		
 	}
+
+	//			try
+	//			{
+	//				
+	//				System.out.println("wszedł w try");
+	//				sessionFactory = new Configuration().configure().buildSessionFactory();
+	//				Session session = sessionFactory.openSession();
+	//			    System.out.println("po konfiguracji");
+	//				session.beginTransaction();
+	//				Query<Message> query = session.createQuery("From Message");
+	//
+	//				messages = query.getResultList();
+	//				session.getTransaction().commit();
+	//				session.close();
+	//
+	//			}
+	//			catch (Exception ex)
+	//			{
+	//				ex.printStackTrace();
+	//				System.out.println("Błąd " + ex.toString());
+	//				session.getTransaction().rollback();
+	//			}
+	//return new ArrayList<Message>(messages1.values());
 
 }
--- messenger/src/main/java/arek/nauka/messenger/database/DataBaseHibernateConn.java
+++ messenger/src/main/java/arek/nauka/messenger/database/DataBaseHibernateConn.java
@@ -6,6 +6,10 @@ import java.util.ArrayList;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
@@ -53,27 +57,46 @@ public class DataBaseHibernateConn
 	public List<Message> getAllMessages()
 	{
 		List<Message> messages = new ArrayList<Message>();
-		try
-		{
-			
-			System.out.println("wszedł w try");
-			sessionFactory = new Configuration().configure().buildSessionFactory();
-			Session session = sessionFactory.openSession();
-		    System.out.println("po konfiguracji");
+		
+		try {
+			StandardServiceRegistry standardRegistry = 
+		       new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+			Metadata metaData = 
+		        new MetadataSources(standardRegistry).getMetadataBuilder().build();
+			sessionFactory = metaData.getSessionFactoryBuilder().build();
+			System.out.println("po konfiguracji");
 			session.beginTransaction();
 			Query<Message> query = session.createQuery("From Message");
 
 			messages = query.getResultList();
 			session.getTransaction().commit();
 			session.close();
-
-		}
-		catch (Exception ex)
-		{
-			ex.printStackTrace();
-			System.out.println("Błąd " + ex.toString());
-			session.getTransaction().rollback();
-		}
+		   } catch (Throwable th) {
+			System.err.println("Enitial SessionFactory creation failed" + th);
+			throw new ExceptionInInitializerError(th);
+		  }
+		
+//		try
+//		{
+//			
+//			System.out.println("wszedł w try");
+//			sessionFactory = new Configuration().configure().buildSessionFactory();
+//			Session session = sessionFactory.openSession();
+//		    System.out.println("po konfiguracji");
+//			session.beginTransaction();
+//			Query<Message> query = session.createQuery("From Message");
+//
+//			messages = query.getResultList();
+//			session.getTransaction().commit();
+//			session.close();
+//
+//		}
+//		catch (Exception ex)
+//		{
+//			ex.printStackTrace();
+//			System.out.println("Błąd " + ex.toString());
+//			session.getTransaction().rollback();
+//		}
 		//return new ArrayList<Message>(messages1.values());
 		return messages;
 
